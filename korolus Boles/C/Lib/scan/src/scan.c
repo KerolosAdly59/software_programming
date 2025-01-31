c6544ade588095dbf674d842d18fd12b8452dbc2
korolus <kerolosadly59@gmail.com>
2025-01-31 21:00:34 +0200

    koko

---
+++ korolus Boles/C/Lib/scan/src/scan.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include "../Header/scan.h"
+
+void scan_number(int x , int y)
+{
+	printf("Enter num1\n ");
+	scanf("%d",&x);
+	printf("Enter num2 ");
+	scanf("%d",&y);
+}
+
+void scan_cahr(char x )
+{	
+		scanf("%c",&x);
+}
+
