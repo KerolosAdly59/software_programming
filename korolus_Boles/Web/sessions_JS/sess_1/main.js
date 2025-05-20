8f6b313a1216d4a15bd27ba0f1d65796e0b33a76
korolusboles <your_email@example.com>
2025-05-20 19:07:36 +0300

    ff

---
+++ korolus_Boles/Web/sessions_JS/sess_1/main.js
@@ -0,0 +1,18 @@
+
+
+var x= Number(prompt("Enter Num-1"));
+var y= Number(prompt("Enter Num-2"));
+
+
+
+console.log(add(x,y));
+
+
+
+
+var x= Number(prompt("Enter Num-1"));
+var y= Number(prompt("Enter Num-2"));
+
+
+console.log(add(x,y));
+
